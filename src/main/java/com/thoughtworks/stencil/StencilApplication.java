456375f5db285d33476a1a3263e5b46c947a6742
Cherish <shmilycherish@gmail.com>
2018-04-22 16:51:05 +0800

    generate spring-boot project via http://start.spring.io/

---
+++ src/main/java/com/thoughtworks/stencil/StencilApplication.java
@@ -0,0 +1,12 @@
+package com.thoughtworks.stencil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StencilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StencilApplication.class, args);
+	}
+}
---
+++ src/test/java/com/thoughtworks/stencil/StencilApplicationTests.java
@@ -0,0 +1,16 @@
+package com.thoughtworks.stencil;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StencilApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
