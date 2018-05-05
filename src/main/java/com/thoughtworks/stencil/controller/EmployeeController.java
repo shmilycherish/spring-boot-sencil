7e63c41e55fc252c295502403747dd87b2801290
Cherish <shmilycherish@gmail.com>
2018-05-05 20:01:38 +0800

    add employee swagger

--- src/main/java/com/thoughtworks/stencil/controller/EmployeeController.java
+++ src/main/java/com/thoughtworks/stencil/controller/EmployeeController.java
@@ -1,12 +1,15 @@
 package com.thoughtworks.stencil.controller;
 
 import com.thoughtworks.stencil.service.EmployeeService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Api(tags = "Employee", description = "Employee Resource")
 public class EmployeeController {
 
     private EmployeeService employeeService;
@@ -16,6 +19,7 @@ public class EmployeeController {
     }
 
     @GetMapping("/employee")
+    @ApiOperation(value = "Get all employees", notes = "This is for get all employees")
     public ResponseEntity getEmployees() {
         return new ResponseEntity<>(employeeService.getAllEmployees(), HttpStatus.OK);
     }
