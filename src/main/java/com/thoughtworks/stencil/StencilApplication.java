ad221eb16655ec3a6eb4eb3bdfea77a61077ab40
Cherish <shmilycherish@gmail.com>
2018-05-05 19:52:52 +0800

    init swaager

--- src/main/java/com/thoughtworks/stencil/StencilApplication.java
+++ src/main/java/com/thoughtworks/stencil/StencilApplication.java
@@ -2,8 +2,10 @@ package com.thoughtworks.stencil;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class StencilApplication {
 
 	public static void main(String[] args) {
